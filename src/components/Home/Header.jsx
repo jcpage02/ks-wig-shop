26fdd004e82a1158b82f022be85ab6d678bc7bfd
James Page <jcpage02@outlook.com>
2019-03-09 10:34:46 -0700

    home page section one complete, minus font family

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import './Header.scss'
-import logo from '../../assets/Flower.png'
-import brand from '../../assets/Group 3.png'
+import React from "react";
+import "./Header.scss";
+import logo from "../../assets/Path 1.svg";
+import brand from "../../assets/Group 3.svg";
 
 export default function Header() {
   return (
-    <div className='header'>
-        <div className='header-branding'>
-            <img className='header-logo' src={logo} alt="logo"/>
-            <img className='header-brand' src={brand} alt="brand"/>
+    <div className="header">
+      <div className="header-branding">
+        <img className="header-logo" src={logo} alt="logo" />
+        <img className="header-brand" src={brand} alt="brand" />
+      </div>
+      <div className="header-nav">
+        <p>BOOK AN APPOINTMENT</p>
+        <div className='header-menu'>
+          <div className="header-menu-lg-line" />
+          <div className="header-menu-sm-line" />
+          <div className="header-menu-lg-line" />
         </div>
-        <div></div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import './Home.scss'
-import imageOne from '../../assets/f81735e6a14d136a57a7b18edbc20e2f@2x.png'
-import Header from './Header'
+import React from "react";
+import "./Home.scss";
+import imageOne from "../../assets/f81735e6a14d136a57a7b18edbc20e2f.svg";
+import downArrow from "../../assets/Group 1.svg";
+import Header from "./Header";
 
 export default function Home() {
   return (
-    <div className='home'> 
+    <div className="home">
       <Header />
-      <div className='home-section-one'>
-        <img src={imageOne} alt=""/>
+      <div className="home-section-one">
+        <img
+          className="home-section-one-img"
+          src={imageOne}
+          alt="girl without hair main"
+        />
+        <div className="home-section-one-footer">
+          <img
+            className="home-section-one-down-arrow"
+            src={downArrow}
+            alt="down"
+          />
+        </div>
+        <div className='home-section-one-msg'>
+          <p>Beautiful. Warrior</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
